24b88d48f0a0ad884800b10ca13f4696606d4f26
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2024-07-06 20:51:50 +0530

    Update telegraph.py

--- plugins/helper/telegraph.py
+++ plugins/helper/telegraph.py
@@ -32,7 +32,7 @@ async def telegraph(bot, message):
     await msg.delete()
     await message.reply_photo(
         photo=f'https://graph.org{response[0]}',
-        caption=f"<b>ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 👇</b>\n\n<code>https://graph.org{response[0]}</code>\n\n<b>ᴘᴏᴡᴇʀᴇᴅ ʙʏ - @bisal_files</b>",       
+        caption=f"<b>ʏᴏᴜʀ ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ ᴄᴏᴍᴘʟᴇᴛᴇᴅ 👇</b>\n\n<code>https://graph.org{response[0]}</code>\n\n<b>ᴘᴏᴡᴇʀᴇᴅ ʙʏ - @TryToLiveAlon</b>",       
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton(text="✓ ᴏᴘᴇɴ ʟɪɴᴋ ✓", url=f"https://graph.org{response[0]}"),
             InlineKeyboardButton(text="📱 sʜᴀʀᴇ ʟɪɴᴋ", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
